6ec2bca6505a73639edf90a91b5b630dd9011d6c
Yuuya1023 <wish.on.the.shootingstar@gmail.com>
2014-10-27 18:06:29 +0900

    config

---
+++ TumblrDesktopApp/config.h
@@ -0,0 +1,20 @@
+//
+//  config.h
+//  TumblrDesktopApp
+//
+//  Created by ___FULLUSERNAME___ on 2014/07/14.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+
+
+
+#define UD_BLOG_NAME @"BLOG_NAME"
+#define UD_DISPLAY_INTERVAL @"DISPLAY_INTERVAL"
+#define UD_IS_ALWAYS_TOP @"IS_ALWAYS_TOP"
+
+#define UD_CACHED_BLOG_NAME @"CACHED_BLOG_NAME"
+
+#define DEFAULT_DISPLAY_INTERVAL @"30"
+
+
+
+#define NOTIF_UPDATE_PREFERENCES @"UPDATE_PREFERENCES"
